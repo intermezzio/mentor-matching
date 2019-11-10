21f4a89867a18e84d18bf61a2ec9d855c9103674
Andrew Mascillaro <amascillaro@gmail.com>
2019-11-09 22:57:45 -0500

    Edit MongoDB Authentication

--- ManDB.py
+++ ManDB.py
@@ -1,5 +1,5 @@
 import pymongo
-
+import urllib
 
 class ManDB:
 	"""
@@ -14,25 +14,28 @@ class ManDB:
 		self.client = pymongo.MongoClient(f"mongodb+srv://{user}:{password or open('keys/loginpass.txt').read()}@cluster0-codlf.mongodb.net/test?retryWrites=true&w=majority")
 		self.user = user
 		self.loginDB = self.client["users"]
+		self.loginDB.authenticate('amascillaro@gmail.com', open('keys/personal_mongodb_pass.txt').read())
 		self.loginCol = self.loginDB["login-info"]
 	
-	def createUser(self, username, password, firstname, lastname, email="e@mail.addr", bio="", school=None, birthdate=25, hobbies=list(), company=None, ethnicity=None, gender=None, position="Lead Systems Engineer", degree="", city="Princeton", state="NJ", ismentor=False, ismentee=False, mentoravailability=False, menteeavailability=False):
-		if self.loginCol.find({"email": email}):
+	def createUser(self, password, firstname, lastname, email, bio="", school=None, birthdate=0, hobbies=list(), company=None, ethnicity=None, gender=None, position="Lead Systems Engineer", degree="", city="Princeton", state="NJ", ismentor=False, ismentee=False, mentoravailability=False, menteeavailability=False):
+		emailSearchCursor = self.loginCol.find({"email": email})
+		foundUsers = [user for user in emailSearchCursor]
+		if foundUsers:
 			raise Exception("That email address is already taken")
 		userProfile = {
-			"username": username,
 			"password": password,
 			"firstname": firstname,
 			"lastname": lastname,
 			"email": email,
 			"bio": bio,
 			"school": school,
+			"birthdate": birthdate,
 			"hobbies": hobbies,
 			"company": company,
 			"ethnicity": ethnicity,
 			"gender": gender,
 			"position": position,
-			"degree": ";".split(degree),
+			"degree": degree,
 			"location": {
 				"city": city,
 				"state": state
@@ -59,3 +62,25 @@ class ManDB:
 			raise Exception("Who are you, really?")
  
 		return get_user
+
+if __name__ == "__main__":
+	m = ManDB()
+	mentee={
+		'password':'123456',
+		'firstname': 'Emilia',
+		'lastname': 'Flores',
+		'email': 'eflores@gmail.com',
+		'school': 'Princeton University',
+		'hobbies':'Baseball',
+		'company': 'Microsoft',
+
+		'ethnicity': 'African American',
+		'gender': 'Female',
+		'position': 'Software Engineer',
+		'degree': 'Mechanical Aerospace Engineer',
+		'city': 'New York City',
+		'state': 'New York',
+		'ismentor': False,
+		'ismentee': True
+	}
+
